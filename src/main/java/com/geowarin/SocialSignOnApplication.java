345d6e336f28d45dd91bb589b45e10e0d7116928
geowarin <geowarin@gmail.com>
2015-05-20 22:07:21 +0200

    Save connection in database

--- src/main/java/com/geowarin/SocialSignOnApplication.java
+++ src/main/java/com/geowarin/SocialSignOnApplication.java
@@ -1,23 +1,37 @@
 package com.geowarin;
 
 import org.h2.tools.Server;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.UserProfile;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
+import org.springframework.social.connect.web.ProviderSignInUtils;
 import org.springframework.social.connect.web.SignInAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
 @SpringBootApplication
+@Controller
 public class SocialSignOnApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SocialSignOnApplication.class, args);
+        new SpringApplicationBuilder(SocialSignOnApplication.class)
+                .showBanner(false)
+                .profiles("dev")
+                .run(args);
     }
 
     @Bean
@@ -27,6 +41,24 @@ public class SocialSignOnApplication {
             return null;
         };
     }
+
+    @RequestMapping(value="/signup")
+    public String signup(WebRequest request) {
+        ProviderSignInUtils providerSignInUtils = new ProviderSignInUtils();
+        Connection<?> connection = providerSignInUtils.getConnectionFromSession(request);
+        if (connection != null) {
+            SocialSignOnApplication.authenticate(connection);
+            providerSignInUtils.doPostSignUp(connection.getDisplayName(), request);
+        }
+        return "index";
+    }
+
+    @Bean
+    @Primary
+    public UsersConnectionRepository getUsersConnectionRepository(DataSource dataSource, ConnectionFactoryLocator connectionFactoryLocator) {
+        return new JdbcUsersConnectionRepository(dataSource, connectionFactoryLocator, Encryptors.noOpText());
+    }
+
 
     public static void authenticate(Connection<?> connection) {
         UserProfile userProfile = connection.fetchUserProfile();
